f0fc6afd281724151ac4d7062e5c95e1abdaabb4
luanvfm <luanventurafm2@gmail.com>
2026-06-16 20:01:30 -0300

    FEATURE: Added struct estoque[3] and function imprimir_produtos (Atividade 4)

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+#include <asm-generic/errno.h>
 #include <stdio.h>
 
 typedef struct Produto {
@@ -19,9 +20,16 @@ void aplicar_desconto(Produto *produto, float porcentDesconto) {
   produto->preco = precoNovo;
 }
 
+void imprimir_produtos(Produto *produto, int quantidadeItems) {
+  for (int i = 0; i < quantidadeItems; i++) {
+    printf("Produto %d \n", (produto + i)->id);
+    printf("Preço: %.2f \n", (produto + i)->preco);
+  }
+}
+
 int main() {
   /* ---- Atividade 1 ----
-    *pricePtr = *pricePtr * 1.40;
+    *pricePtr = *pricePtr * 1.40;B
     printf("%.2f\n", price);
   */
 
@@ -30,6 +38,7 @@ int main() {
   float *precoPtr = &preco;
   */
 
+  /* Parte da atividade 3
   Produto produto;
   produto.id = 1;
   produto.preco = 100;
@@ -37,4 +46,19 @@ int main() {
   aplicar_desconto(&produto, 10);
 
   printf("%.2f\n", produto.preco);
+  */
+
+  Produto estoque[3];
+  // Inicializar estoque
+  for (int i = 0; i < 3; i++) {
+    if (i == 0) {
+      estoque[i].id = i + 1;
+      estoque[i].preco = 100;
+    } else {
+      estoque[i].id = i + 1;
+      estoque[i].preco = estoque[i - 1].preco + 10;
+    }
+  }
+
+  imprimir_produtos(estoque, 3);
 }
