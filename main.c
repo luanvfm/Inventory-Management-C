a37aeca1b877d726a5a10c0da57c573373e5dc33
luanvfm <luanventurafm2@gmail.com>
2026-06-16 19:04:33 -0300

    FEAT: Added aplicar_desconto function (atividade 2)

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
 
-float price = 100.0;
-float *pricePtr = &price;
+void aplicar_desconto(float *preco, float porcentDesconto) {
+  float desconto = *preco * (porcentDesconto / 100);
+  float precoNovo = *preco - desconto;
+  *preco = precoNovo;
+}
 
 int main() {
-  *pricePtr = *pricePtr * 1.40;
-  printf("%.2f\n", price);
+  // Atividade 1
+  //  *pricePtr = *pricePtr * 1.40;
+  //  printf("%.2f\n", price);
+  float preco = 100.0;
+  float *precoPtr = &preco;
+  aplicar_desconto(&preco, 10);
+
+  printf("%.2f\n", preco);
 }
